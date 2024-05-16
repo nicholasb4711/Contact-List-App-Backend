ec22def87ccdadada13e5891a27d664b2fee26b0
nicholasb4711 <byrne.ni@northeastern.edu>
2024-05-16 08:59:10 -0400

    basic backend API complete

--- main.py
+++ main.py
@@ -18,6 +18,29 @@ def create_contact():
     
     if not first_name or not last_name or not email:
         return jsonify({"message": "You must include a first name, last name, and email"}), 400
+    
+    new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
+    try:
+        db.session.add(new_contact)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+    
+    return jsonify({"message": "User created successfully"}), 201
+
+
+@app.route('/update_contact/<int:user_id>', methods=['PATCH'])
+def update_contact(user_id):
+    '''Update a contact.'''
+    contact = Contact.query.get(user_id)
+    if not contact:
+        return jsonify({"message": "User not found"}), 404
+    data = request.json
+    # Update the contact
+    contact.first_name = data.get('firstName', contact.first_name)
+    contact.last_name = data.get('lastName', contact.last_name)
+    contact.email = data.get('email', contact.email)
+        
 
 if __name__ == '__main__':
     '''Run the app.'''
